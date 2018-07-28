18b6554010afb1c363e16b5034e5d9ae2144195f
Tank <fuch@fullsample.com>
2018-07-28 08:51:50 +0800

    [fuchun] #Tech add monitor log info for thread access add/remove socket

--- src/main/java/com/tank/common/SocketContainer.java
+++ src/main/java/com/tank/common/SocketContainer.java
@@ -1,6 +1,8 @@
 package com.tank.common;
 
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -13,16 +15,22 @@ import java.util.concurrent.CopyOnWriteArrayList;
 /**
  * @author fuchun
  */
+@Slf4j
 @Component
 public class SocketContainer {
 
 
-  public void addSocket(@NonNull WebSocketSession socket) {
+  public synchronized void addSocket(@NonNull WebSocketSession socket) {
+    val threadName = Thread.currentThread().getName();
+    log.info("thread " + threadName + "start add socket");
     sessions.add(socket);
     sockets.putIfAbsent(socket.getId(), socket);
+    log.info("thread " + threadName + "end add socket");
   }
 
-  public void removeSocket(@NonNull WebSocketSession socket) {
+  public synchronized void removeSocket(@NonNull WebSocketSession socket) {
+    val threadName = Thread.currentThread().getName();
+    log.info("thread " + threadName + "start remove socket");
     sockets.remove(socket.getId());
     int index = -1;
     for (int i = 0; i < sessions.size(); i++) {
@@ -34,6 +42,7 @@ public class SocketContainer {
     if (index != -1) {
       sessions.remove(index);
     }
+    log.info("thread " + threadName + "end remove socket");
   }
 
   public int onlineUsers() {
